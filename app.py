980face0ea9d464fb2b22f2cc3445a53ec970906
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-08-18 17:23:55 -0500

    add read inventory.csv, clean date and quantity

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
 from models import (Base, session,
                     Product,engine)
+import csv
+import datetime
+import os  
 
 
+def clean_date(date_str):
+    '''Turns the string into a date object'''
+    
+    split_date=date_str.split('/')
+    month=int(split_date[0])
+    day=int(split_date[1])
+    year=int(split_date[2])
+    
+    
 
+    return datetime.date(year,month,day)
+
+def clean_quantity(qty_str):
+   
+    return int(qty_str)
+
+
+def clean_price(price_str):
+    pass
+   
+   
+    
+  
+    
+
+def add_csv():
+    with open('inventory.csv') as csvfile:
+        data=csv.DictReader(csvfile)
+       
+        for row in data:
+            product_name=row['product_name']
+            product_price=clean_price(row['product_price'])
+            product_quantity=clean_quantity(row['product_quantity'])
+            date=clean_date(row['date_updated'])
+         
+            
+          
+          
 
 if __name__=='__main__':
-    Base.metadata.create_all(engine)
+    #Base.metadata.create_all(engine)
+    add_csv()
+    #clean_date('11/1/2018')
