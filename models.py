aad7a2a4acafb24600bb032f3cd795522317a25a
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-08-17 23:31:48 -0500

    add product model

--- models.py
+++ models.py
@@ -1,13 +1,53 @@
-from sqlalchemy import create_engine
+import datetime
+
+from sqlalchemy import (create_engine, Column, Integer,
+                        String, Date)
+from sqlalchemy.orm import (DeclarativeBase,
+                            mapped_column)
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import sessionmaker
+
+
+
+
+
+
 
 # Initialize Sqlite database
-engine=create_engine('sqlite://inventory.db')
+engine=create_engine('sqlite:///inventory.db', echo=True)
+#on sqlachemy 2.0 declarative_base has been replaced by Declarative Base
+#create the Declarative Base
+
+#this is the new form as per sqlalchemy 2.0
+class Base(DeclarativeBase):
+    pass
+
+Session=sessionmaker(engine)
+session=Session()
+    
+
 
 # Create your database model called Product
+class Product(Base):
+    
+    __tablename__='inventory'
 
-## Attributes
-###  Product_id=Primary_key, 
-###  product_name, 
-###  product_quantity, 
-###  product_price,
-###  date_updated.
+    # Attributes
+    # Product_id=Primary_key,
+    # product_name,
+    # product_quantity,
+    # product_price,
+    # date_updated.
+    product_id = Column('Product Id',Integer, primary_key=True)
+    product_name = Column('Product Name',String)
+    product_quantity = Column('Product Quantity',Integer)
+    product_price = Column('Product Price',Integer)
+    date_updated = Column('Date Updated',Date)
+
+
+    def __repr__(self) -> str:
+        ... 
+        return f"'Product Name:{self.product_name}, Quantity: {self.product_quantity}, Price: {self.product_price},Date Updated: {self.date_updated} "
+
+    
+
