e3db08ddc374c435aca5a2f60c3dfc9826e03958
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-08-22 22:11:51 -0500

    add products to the database.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from models import (Base, session,
-                    Product,engine)
+                    Product, engine)
 import csv
+import sys
 import datetime
-import os  
+import os
 
 
 def menu():
@@ -13,11 +14,11 @@ def menu():
 
             \n* View a single product\'s inventory(v)
             \r*Add a new product to the database (a)
-            \r* Make a backup of the entire inventory(b)
-            
+            \r*Make a backup of the entire inventory(b)
+
             ''')
-        choice=input('What would you like to do?: ')
-        if choice in ['v','a','b']:
+        choice = input('What would you like to do?: ')
+        if choice in ['v', 'a', 'b']:
             return choice
         else:
             input(
@@ -25,74 +26,91 @@ def menu():
                 \rPlease enter one of the options above
                 \rletters a, b or v only
                 \rPress enter to try again''')
-        
-            
-       
+
+
+def view_product():
+    product = input('Please enter the id of the product ')
+    pass
+
+
+def add_product():
+    pass
+
+
+def create_backup():
+    pass
 
 
 def clean_date(date_str):
     '''Turns the string into a date object'''
-    
-    split_date=date_str.split('/')
-    month=int(split_date[0])
-    day=int(split_date[1])
-    year=int(split_date[2])
-    
-    
 
-    return datetime.date(year,month,day)
+    split_date = date_str.split('/')
+    month = int(split_date[0])
+    day = int(split_date[1])
+    year = int(split_date[2])
+
+    return datetime.date(year, month, day)
+
 
 def clean_quantity(qty_str):
-   
+
     return int(qty_str)
 
 
 def clean_price(price_str):
-    price_float=float(price_str)
-    return int(price_float*100)
+    #print(price_str)
+    split_price=price_str.split('$')
+   
+    price_float =float(split_price[1])
+    
+    
+    return int(price_float *100)
 
 
 def add_csv():
     with open('inventory.csv') as csvfile:
-        data=csv.DictReader(csvfile)
-       
+        data = csv.DictReader(csvfile)
+
         for row in data:
-            product_name=row['product_name']
-            product_price=clean_price(row['product_price'])
-            product_quantity=clean_quantity(row['product_quantity'])
-            date=clean_date(row['date_updated'])
+            product_name = row['product_name']
+            product_price = clean_price(row['product_price'])
+            product_quantity = clean_quantity(row['product_quantity'])
+            date_updated = clean_date(row['date_updated'])
+            new_product=Product(product_name = product_name, product_quantity = product_quantity, product_price = product_price, date_updated = date_updated )
+            session.add(new_product)
+        session.commit()
+
 
 def app():
-    app_running=True
+    app_running = True
     while app_running:
-        choice=menu()
-        if choice=='v':
-            #view a single product
-            
+        choice = menu()
+        if choice == 'v':
+            # view a single product
+            view_product()
             pass
-        elif choice=='a':
+
+        elif choice == 'a':
             # add a product
             pass
-       
 
-
-        elif choice=='b':
+        elif choice == 'b':
             # create backup
             pass
-
         else:
-            'Good bye'
-            app_running=False
-            
-           
-         
-            
-          
-          
+            app_running = False
+            input(
+                '''
+                \rPlease enter one of the options above
+                \rletters a, b or v only
+                \rPress enter to try again: ''')
 
-if __name__=='__main__':
+      
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
     app()
-    #Base.metadata.create_all(engine)
-    #menu()
-    #add_csv()
-    #clean_date('11/1/2018')
+ 
+   
+    
